6f527edc3c7da7590eaedc1bb4265ca99c0491b4
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-12-27 16:31:22 +0700

    Update readapis.js

--- readapis.js
+++ readapis.js
@@ -1,4 +1,4 @@
-
+inc <api.json = parse>
 inc <sys.h>;
 inc <nes.h>;
 var prn = awat function(data){
@@ -18,3 +18,11 @@ var chk = web3.open("api.json");
 var psr = neonapi.parse(chk,ur);
 prn.data(psr,chk,null);
 //parse in wigher neoa sna
+cns = sol;
+nus = nam(
+  "scriptor inc"
+);
+wiget = prn.data.read(psr,chk,null);
+app.title(nus);
+app.widget(wiget);
+git.read("https://github.com/awwsssssssss/api.js")
