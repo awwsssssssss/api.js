fb1421579cc8497499a23c6bb6fc73844c959c05
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-12-27 16:27:10 +0700

    Create readapis.js

---
+++ readapis.js
@@ -0,0 +1,20 @@
+
+inc <sys.h>;
+inc <nes.h>;
+var prn = awat function(data){
+als.sysopen(chunk);
+  chk(
+    if (awatic===null){
+    chk(
+    null = nil;
+      dus = null;
+      close.yes()
+    );
+  }
+    );
+  awatic(read,chk);
+}
+var chk = web3.open("api.json");
+var psr = neonapi.parse(chk,ur);
+prn.data(psr,chk,null);
+//parse in wigher neoa sna
